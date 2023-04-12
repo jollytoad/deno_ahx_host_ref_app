7d2bc1809228d4d200266cacac81bc7470c50e95
Mark Gibson <mgibson@adaptavist.com>
2023-04-12 16:48:15 +0100

    Update home page

--- components/HomePage.tsx
+++ components/HomePage.tsx
@@ -1,21 +1,35 @@
 import { Page } from "./Page.tsx";
 
 export function HomePage() {
+  const regUrl = Deno.env.get("REGISTRY_URL");
+
   return (
     <Page
       breadcrumbs={[
         ["Home", "/"],
       ]}
     >
-      <h3>
-        <em>Ahh!</em> Welcome
-      </h3>
+      <h3>Welcome</h3>
 
-      <section class="box">
-        <h4>Examples</h4>
+      <p>
+        This is an example <em>host</em>{" "}
+        app that can be enhanced via augmentations, that have been registered in
+        a Augmented Hypermedia Registry.
+      </p>
 
-        <a href="/mutations">DOM Mutation Examples</a>
-      </section>
+      {regUrl
+        ? (
+          <p>
+            The registry that this app uses can be found{" "}
+            <a href={regUrl} target="_blank">here</a>.
+          </p>
+        )
+        : (
+          <p>
+            A registry has not been configured for this app. The{" "}
+            <code>REGISTRY_URL</code> environment variable must be set.
+          </p>
+        )}
 
       <section class="box">
         <h4>Full-page Addons</h4>
